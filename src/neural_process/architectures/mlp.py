bc6c7dbc8780ebafad0c7c98c0ae211837dc8068
ls1221 <ls1221@ic.ac.uk>
2024-12-21 17:33:23 +0000

    Refactor mlp

--- src/neural_process/architectures/mlp.py
+++ src/neural_process/architectures/mlp.py
@@ -1,7 +1,9 @@
+"""MLP module for Neural Process"""
+
 import torch.nn as nn
 
 
-class MLP(nn.module):
+class MLP(nn.Module):
     """General MLP Class
 
     Parameters
@@ -12,22 +14,22 @@ class MLP(nn.module):
     output_size : int
         Size of the output
 
-    hidden_size : int
+    hidden_size : int (optional)
         Size of the hidden layers
 
-    n_hidden_layers : int
+    n_hidden_layers : int (optional)
         Number of hidden layers
 
-    activation : torch.nn.Module
+    activation : torch.nn.Module (optional)
         Activation function to use
 
-    is_bias : bool
+    is_bias : bool (optional)
         Whether to use bias in the linear layers
 
-    dropout : float
+    dropout : float (optional)
         Dropout rate
 
-    is_res : bool
+    is_res : bool (optional)
         Whether to use residual connections
     """
 
@@ -37,47 +39,47 @@ class MLP(nn.module):
         output_size,
         hidden_size=32,
         n_hidden_layers=1,
-        activation=nn.Relu(),
+        activation=nn.ReLU(),
         is_bias=True,
         dropout=0.1,
         is_res=False,
     ):
         super().__init__()
 
-        self._input_size = input_size
-        self._output_size = output_size
-        self._hidden_size = hidden_size
-        self._n_hidden_layers = n_hidden_layers
-        self._activation = activation
-        self._is_res = is_res
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_hidden_layers = n_hidden_layers
+        self.activation = activation
+        self.is_res = is_res
 
-        self._dropout = nn.Dropout(p=dropout)
+        self.dropout = nn.Dropout(p=dropout)
 
-        self._to_hidden = nn.Linear(self.input_size, self.hidden_size, bias=is_bias)
-        self._linears = nn.ModuleList(
+        self.to_hidden = nn.Linear(self.input_size, self.hidden_size, bias=is_bias)
+        self.linears = nn.ModuleList(
             [
-                nn.Linear(self._hidden_size, self._hidden_size, bias=is_bias)
-                for _ in range(self._n_hidden_layers - 1)
+                nn.Linear(self.hidden_size, self.hidden_size, bias=is_bias)
+                for _ in range(self.n_hidden_layers - 1)
             ]
         )
-        self._out = nn.Linear(self._hidden_size, self._output_size, bias=is_bias)
+        self.out = nn.Linear(self.hidden_size, self.output_size, bias=is_bias)
 
         # Initialise weights with Kaiming for RELU for now
-        nn.init.kaiming_uniform_(self._to_hidden.weight, nonlinearity="relu")
-        for linear in self._linears:
+        nn.init.kaiming_uniform_(self.to_hidden.weight, nonlinearity="relu")
+        for linear in self.linears:
             nn.init.kaiming_uniform_(linear.weight, nonlinearity="relu")
-        nn.init.kaiming_normal_(self._out.weight, nonlinearity="relu")
+        nn.init.kaiming_normal_(self.out.weight, nonlinearity="relu")
 
     def forward(self, x):
-        x = self._activation(self._to_hidden(x))
-        res = self._dropout(x)
+        x = self.activation(self.to_hidden(x))
+        res = self.dropout(x)
 
-        for linear in self._linears:
-            x = self._activation(linear(res))
-            if self._is_res:
+        for linear in self.linears:
+            x = self.activation(linear(res))
+            if self.is_res:
                 x = x + res
-            x = self._dropout(x)
+            x = self.dropout(x)
             res = x
 
-        x = self._out(res)
+        x = self.out(res)
         return x
